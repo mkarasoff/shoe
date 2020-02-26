98df35f530f63a2b346f28aae26dbda65d0b2c90
Michael Karasoff <3068036+mkarasoff@users.noreply.github.com>
2020-02-25 21:10:43 -0800

    Added xml for old firmware to try to get -F option to work

---
+++ shoelib/shoeVer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+##########################################################################
+#SHOE - An open source HEOS configuration and control project
+#Copyright (C) 2020  Mike Karasoff, mike@karatronics.com
+#
+#Top level script.  Run This!
+#
+##########################################################################
+#GPLv.3 License
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##########################################################################
+
+SHOE_VERSION="0.1.7"
